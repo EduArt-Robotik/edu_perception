d84001886162c9a2ff5ac0aa9694a7c1d5e5bec0
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-03-14 09:18:45 +0000

    Implement minimal example for testing depthai and opencv library.

--- src/node/qr_detection_and_pose_estimation_node.cpp
+++ src/node/qr_detection_and_pose_estimation_node.cpp
@@ -4,6 +4,9 @@
 #include <rclcpp/node.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <depthai/depthai.hpp>
+#include <opencv2/opencv.hpp>
+
 namespace eduart {
 namespace perception {
 
@@ -34,6 +37,42 @@ QrDetectionAndPoseEstimation::~QrDetectionAndPoseEstimation()
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
+
+    using namespace std;
+
+    // Create pipeline
+    dai::Pipeline pipeline;
+    auto colorCam = pipeline.create<dai::node::ColorCamera>();
+    auto xlinkOut = pipeline.create<dai::node::XLinkOut>();
+    xlinkOut->setStreamName("preview");
+    colorCam->setInterleaved(true);
+    colorCam->preview.link(xlinkOut->input);
+
+
+    try {
+        // Try connecting to device and start the pipeline
+        dai::Device device(pipeline);
+
+        // Get output queue
+        auto preview = device.getOutputQueue("preview");
+
+        cv::Mat frame;
+        while (true) {
+
+            // Receive 'preview' frame from device
+            auto imgFrame = preview->get<dai::ImgFrame>();
+
+            // Show the received 'preview' frame
+            cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
+
+            // Wait and check if 'q' pressed
+            if (cv::waitKey(1) == 'q') return 0;
+
+        }
+    } catch (const std::runtime_error& err) {
+        std::cout << err.what() << std::endl;
+    }
+
   rclcpp::spin(std::make_shared<eduart::perception::QrDetectionAndPoseEstimation>());
   rclcpp::shutdown();
 
